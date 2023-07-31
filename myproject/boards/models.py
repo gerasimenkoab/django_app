5a3204e4cd0dca46a6a1ef9c19cd029bb3b00c3c
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-07-31 12:15:00 +0200

    add template

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+"""Objects for database formation with makemigrations
+    $python manage.py makemigrations
+    $python manage.py sqlmigrate boards 0001
+    $python manage.py migrate
+"""
+class Board(models.Model):
+    name = models.CharField(max_length=100, unique = True)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=100)
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, on_delete = models.CASCADE, related_name='topics')
+    starter = models.ForeignKey(User, on_delete = models.CASCADE, related_name='topics')
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    message = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(null=True)
+    topic = models.ForeignKey(Topic, on_delete = models.CASCADE, related_name='posts')
+    created_by = models.ForeignKey(User, on_delete= models.CASCADE, related_name='posts')
+    updated_by = models.ForeignKey(User, null=True, on_delete = models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return self.name
+
+
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 def home(request):
     """View  with a simple response."""
-    return HttpResponse("Hello, people!")
+    boards = Board.objects.all()
+    # board_names = list()
+    # for board in boards:
+    #     board_names.append(board.name)
+
+    # responseHTML = '<br>'.join(board_names)
+
+    # return HttpResponse(responseHTML)
+    return render(request, 'home.html',{'boards':boards})
