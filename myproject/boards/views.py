dfef589aa6f6345199cc9989dd0c1d77adf149e4
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-10-03 23:10:32 +0200

    add new post message control

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Board
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -21,4 +22,25 @@ def about(request):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, "new_topic.html",{'board':board})
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: get the currently logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return render(request, 'topics.html', {'board': board})
+        #return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+
+    return render(request, 'new_topic.html', {'board': board})
