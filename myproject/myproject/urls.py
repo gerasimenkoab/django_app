76501ab87debd758c2baaf1639e36a104bfeab62
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-10-05 22:58:02 +0200

    add user authentication

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from boards import views
+from accounts import views as accounts_views
 #from django.conf.urls import url # deprecated after Django 4.0
 
 urlpatterns = [
     re_path(r'^$',views.home, name = 'home'),
+    re_path(r'^signup/$', accounts_views.signup, name = 'signup'),
     re_path(r'^about/',views.about, name='about'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
