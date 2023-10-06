444f048c3584c135191024b1dba8c043fb286f76
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-10-06 23:40:08 +0200

    add login

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -24,6 +24,7 @@ from accounts import views as accounts_views
 urlpatterns = [
     re_path(r'^$',views.home, name = 'home'),
     re_path(r'^signup/$', accounts_views.signup, name = 'signup'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^about/',views.about, name='about'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
